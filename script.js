491aec2d5fee679389b02c665d00485004e75a0d
techwiz-web <wutchechikaome75@gmail.com>
2022-08-02 14:31:33 +0100

    fhh

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const name = "ada";
 const palidrone = "";
 for (i = name.length - 1; i >= 0; i--) {
 	palidrone += name[i].toLowerCase();
+	console.log(palidrone);
 }
 if (name.toLowerCase() == palidrone) {
 	console.log("true");
