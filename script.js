57b9f5f21f8511657a0caff6377b5615ac903348
techwiz-web <wutchechikaome75@gmail.com>
2022-08-02 14:58:26 +0100

    nf

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /** @format */
 //01 palidrone
-// const name = "ada";
-// var palidrone = "";
-// for (i = name.length - 1; i >= 0; i--) {
-// 	palidrone += name[i].toLowerCase();
-// }
-// if (name.toLowerCase() == palidrone) {
-// 	console.log(true);
-// } else {
-// 	console.log(false);
-// }
+const name = "ada";
+var palidrone = "";
+for (i = name.length - 1; i >= 0; i--) {
+	palidrone += name[i].toLowerCase();
+}
+if (name.toLowerCase() == palidrone) {
+	console.log(true);
+} else {
+	console.log(false);
+}
 //02
 var array = [2, 10, 5, 4];
 var newArr = [];
